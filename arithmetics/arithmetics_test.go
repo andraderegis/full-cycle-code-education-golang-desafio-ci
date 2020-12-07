2f863fe29c18adc5997139790e62df389febe668
andraderegis <reginaldo.andrade@outlook.com>
2020-12-07 15:11:11 -0300

    fix import

--- arithmetics/arithmetics_test.go
+++ arithmetics/arithmetics_test.go
@@ -1,13 +1,12 @@
 package arithmetics
 
 import (
-	utils "golang-fullcycle-desafio-ci/utils"
 	"testing"
 )
 
 func TestExpectedSum(t *testing.T) {
 	const sumExpected int = 10
-	sumResult := utils.Sum(5, 5)
+	sumResult := Sum(5, 5)
 
 	if sumResult != sumExpected {
 		t.Errorf("Expected result is %d, but result is %d", sumExpected, sumResult)
--- arithmetics/go.mod
+++ arithmetics/go.mod
@@ -1,1 +1,3 @@
 module github.com/andraderegis/golang-fullcycle-desafio-ci/arithmetics
+
+go 1.15
